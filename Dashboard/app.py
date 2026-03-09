779407ba586f7aec447b581e76500f979c23e4ca
leonseehaojun <leonsee1000@yahoo.com.sg>
2026-03-09 12:18:44 +0800

    File Organisation

---
+++ Dashboard/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from Dashboard.mqtt_listener import start_mqtt, latest_data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", data=latest_data)
+
+if __name__ == "__main__":
+    start_mqtt()
+    app.run(host="0.0.0.0", port=5000, debug=True)
